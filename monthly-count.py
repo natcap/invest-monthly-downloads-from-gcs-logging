26891e49139fadc94bd41ef132dce8f6a87faaee
James Douglass <jamesdouglassusa@gmail.com>
2020-08-17 17:53:47 -0700

    A few tweaks.

--- monthly-count.py
+++ monthly-count.py
@@ -3,24 +3,45 @@ import datetime
 import glob
 import pprint
 import re
+import time
 
 import pandas
 
 
 def count():
     monthly_counts = collections.defaultdict(int)
+    last_time = time.time()
+    n_files_touched = 0
+
+    summary_file = open('summary.txt', 'w')
     for usage_file in glob.glob(
             'logging/releases.naturalcapitalproject.org/_usage*'):
-        table = pandas.read_csv(usage_file)
+        n_files_touched += 1
+        if time.time() - last_time > 5.0:
+            print(f'{n_files_touched} so far')
+            last_time = time.time()
 
-        year_month_day = re.findall('[0-9]{4}_[0-9]+_[0-9]+', usage_file)
-        date = datetime.date(*year_month_day.split('_'))
+        table = pandas.read_csv(usage_file, sep=None, engine='python')
 
-        for row in table.iterrows():
+        year_month_day = re.findall('[0-9]{4}_[0-9]+_[0-9]+', usage_file)[0]
+        date = datetime.date(*[int(s) for s in year_month_day.split('_')])
+
+        for index, row in table.iterrows():
             downloaded_file = row['cs_object']
-            if ('InVEST' in downloaded_file and
-                    downloaded_file.endswith(('.exe', '.dmg', '.zip'))):
-                print(downloaded_file)
-                monthly_counts[f'{date.year}-{date.month}'] += 1
+            if not isinstance(downloaded_file, str):
+                # sometimes is nan
+                continue
 
-    pprint.pprint(monthly_counts)
+            if ('InVEST' in downloaded_file and
+                    downloaded_file.endswith(('.exe', '.dmg', '.zip')) and
+                    'userguide' not in downloaded_file):
+                monthly_counts[f'{date.year}-{date.month}'] += 1
+                summary_file.write(
+                    f'{date.year}-{date.month}-{date.day},{downloaded_file}\n')
+
+    summary_file.close()
+    pprint.pprint(dict(monthly_counts))
+
+
+if __name__ == '__main__':
+    count()
